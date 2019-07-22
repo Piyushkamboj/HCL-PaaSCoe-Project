54eacc21204e9d01dee9a776d907a46f1db679e1
SauravSrivastav <sauravsrivastav2205@gmail.com>
2019-07-22 12:38:31 +0530

    first commit

---
+++ Data/EmployeesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HCLPaasCoe.Models
+{
+    public class EmployeesContext : DbContext
+    {
+        public EmployeesContext (DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<HCLPaasCoe.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HCLPaasCoe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
